4229f838b6029b6e0a58b20cb2b60cb148fd3838
Howard Ames III <102706916+Shiloh-Ames@users.noreply.github.com>
2022-07-01 19:35:33 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// Nav hamburgerburger selections
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("header nav");
+const nav = document.querySelector("header");
+
+// Scroll to top selection
+const scrollUp = document.querySelector("#scroll-up");
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+// Hamburger menu function
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+// Close hamburger menu when a link is clicked
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
